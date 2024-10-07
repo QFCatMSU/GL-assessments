00ccf29296c9fa18f7e83546609522952ed79478
lidach <lisa.chong8594@gmail.com>
2024-10-07 10:25:56 -0400

    fix merge problems

--- 1836_Treaty_Waters/whitefish/wf.r
+++ 1836_Treaty_Waters/whitefish/wf.r
@@ -29,27 +29,19 @@ if(data$qt_ctl == "time-varying")
 { 
   par$log_qt_devs = matrix(0, nrow = data$n_year - 1, ncol = data$n_fleet)
   par$log_qt_sd = rep(log(0.05), data$n_fleet)
-  par$log_qt_devs = matrix(0, nrow = data$n_year - 1, ncol = data$n_fleet)
-  par$log_qt_sd = rep(log(0.05), data$n_fleet)
 }else 
 {
   par$log_qt_devs = numeric(0)
   par$log_qt_sd = numeric(0)
-  par$log_qt_devs = numeric(0)
-  par$log_qt_sd = numeric(0)
 }
-par$log_ct_sd = rep(log(0.05), data$n_fleet) # standard deviations for catch 
-par$log_r_sd = 0 # standard deviation for recruitment deviations
 par$log_ct_sd = rep(log(0.05), data$n_fleet) # standard deviations for catch 
 par$log_r_sd = 0 # standard deviation for recruitment deviations
 # autocorrelation parameter
 if(data$rec_ctl == "AR1") 
 {
   par$t_phi = 0
-  par$t_phi = 0
 }else 
 {
-  par$t_phi = numeric(0)
   par$t_phi = numeric(0)
 }
 
@@ -57,7 +49,6 @@ if(data$rec_ctl == "AR1")
 ########################
 ## Assessment model ####
 ########################
-f = function(par) 
 f = function(par) 
 {
   # data list from global to local environment
@@ -78,8 +69,6 @@ f = function(par)
   # objective function:
   jnll = 0 # joint negative log likelihood
   nll = numeric(4) # individual negative log likelihoods - catchability, recruitment, catch, proportions at age
-  jnll = 0 # joint negative log likelihood
-  nll = numeric(4) # individual negative log likelihoods - catchability, recruitment, catch, proportions at age
 
   # selectivity
   sel = array(0, dim = c(n_year, n_age, n_fleet))
@@ -88,14 +77,13 @@ f = function(par)
     for(t in 1:n_year) 
     {
       sel[t,,f] = selectivity(sel_ctl = sel_ctl[f],
-              input = la[t,],
-              sel_pars = sel_pars[,f])
+              input = la[t, ],
+              sel_pars = sel_pars[ ,f])
     }
   }
   
 
   # catchability
-  log_qt = matrix(0, nrow = n_year, ncol = n_fleet)
   log_qt = matrix(0, nrow = n_year, ncol = n_fleet)
   for(f in 1:n_fleet)
   {
@@ -103,11 +91,9 @@ f = function(par)
     {
       # initialize q
       log_qt[1,f] = log_q[f]
-      log_qt[1,f] = log_q[f]
       # walk through remaining timesteps for q
       for(t in 2:n_year) 
       {
-        log_qt[t,f] = log_qt[t - 1, f] + log_qt_devs[t - 1, f]
         log_qt[t,f] = log_qt[t - 1, f] + log_qt_devs[t - 1, f]
       }
       # likelihood for catchability deviations
@@ -123,13 +109,10 @@ f = function(par)
 
   # calculate mortalities
   F = array(0, dim = c(n_year, n_age, n_fleet))
-  F = array(0, dim = c(n_year, n_age, n_fleet))
   for(f in 1:n_fleet) 
   {
     F[,,f] = exp(log_qt[,f]) * obs_eff[,f] * sel[,,f]
-    F[,,f] = exp(log_qt[,f]) * obs_eff[,f] * sel[,,f]
   }
-  Z = apply(F, MARGIN = c(1,2), FUN = sum) + M
   Z = apply(F, MARGIN = c(1,2), FUN = sum) + M
 
   # recruitment
@@ -141,21 +124,15 @@ f = function(par)
     for (t in 2:n_year) 
     {
       nll[2] = nll[2] - dnorm(log_r[t], log_r[t - 1], r_sd, TRUE)
-      nll[2] = nll[2] - dnorm(log_r[t], log_r[t - 1], r_sd, TRUE)
     }
   }else if (rec_ctl == "AR1") # AR-1
   { 
-    stationary_sd = sqrt(r_sd * r_sd / (1 - phi * phi))
-    nll[2] = nll[2] - dautoreg(log_r, phi = phi, scale = stationary_sd, log = TRUE)
     stationary_sd = sqrt(r_sd * r_sd / (1 - phi * phi))
     nll[2] = nll[2] - dautoreg(log_r, phi = phi, scale = stationary_sd, log = TRUE)
   }
 
   # population model, including plus group
   # initialize log_n
-  log_n = matrix(-20, nrow = n_year, ncol = n_age)
-  log_n[, 1] = log_r
-  log_n[1, 2:(length(log_n_init) + 1)] = log_n_init
   log_n = matrix(-20, nrow = n_year, ncol = n_age)
   log_n[, 1] = log_r
   log_n[1, 2:(length(log_n_init) + 1)] = log_n_init
@@ -166,10 +143,8 @@ f = function(par)
     for (a in 2:n_age) 
     {
       log_n[t, a] = log_n[t - 1, a - 1] - Z[t - 1, a - 1]
-      log_n[t, a] = log_n[t - 1, a - 1] - Z[t - 1, a - 1]
     }
     #advancing + ( * ) there already
-    log_n[t, n_age] = log(exp(log_n[t, n_age]) +     # advancing
     log_n[t, n_age] = log(exp(log_n[t, n_age]) +     # advancing
                            exp(log_n[t - 1, n_age]) * exp(-Z[t - 1, n_age]))
                                 # there already
@@ -179,14 +154,8 @@ f = function(par)
   ct = array(0, dim = c(n_year, n_age, n_fleet))
   ct_total = matrix(0, nrow = n_year, ncol = n_fleet)
   pa = array(0, dim = c(n_year, n_age, n_fleet))
-  ct = array(0, dim = c(n_year, n_age, n_fleet))
-  ct_total = matrix(0, nrow = n_year, ncol = n_fleet)
-  pa = array(0, dim = c(n_year, n_age, n_fleet))
   for(f in 1:n_fleet) 
   {
-    ct[,,f] = F[,,f] / Z * (1 - exp(-Z)) * exp(log_n)
-    ct_total[,f] = rowSums(ct[,,f])
-    pa[,,f] = ct[,,f] / ct_total[,f]
     ct[,,f] = F[,,f] / Z * (1 - exp(-Z)) * exp(log_n)
     ct_total[,f] = rowSums(ct[,,f])
     pa[,,f] = ct[,,f] / ct_total[,f]
@@ -194,20 +163,15 @@ f = function(par)
 
   # likelihoods
   neff_dm = matrix(0, nrow = n_year, ncol = n_fleet)
-  neff_dm = matrix(0, nrow = n_year, ncol = n_fleet)
   for (f in unique(fleet)) 
   {
     for (y in unique(year)) 
     {
       t = which(years == y)
-      t = which(years == y)
       # catch likelihood
       idx = which(fleet == f & year == y & type == "ct")
       nll[3] = nll[3] - dnorm(obs[idx], ct_total[t,f], ct_sd[f], log = TRUE)
-      idx = which(fleet == f & year == y & type == "ct")
-      nll[3] = nll[3] - dnorm(obs[idx], ct_total[t,f], ct_sd[f], log = TRUE)
       # proportions at age likelihood
-      idx = which(fleet == f & year == y & type == "pa")
       idx = which(fleet == f & year == y & type == "pa")
       if (length(idx) != 0 & sum(obs[idx]) > 0) 
       {
@@ -215,14 +179,11 @@ f = function(par)
         {
           nll[4] = nll[4] - ddirmultinom(obs[idx], pa[t,,f] + 1e-8, bio_samp[t,f], theta[f])
           neff_dm[t,f] = 1 / (1 + theta[f]) + ess[t,f] * (theta[f] / (1 + theta[f]))
-          nll[4] = nll[4] - ddirmultinom(obs[idx], pa[t,,f] + 1e-8, bio_samp[t,f], theta[f])
-          neff_dm[t,f] = 1 / (1 + theta[f]) + ess[t,f] * (theta[f] / (1 + theta[f]))
         }
       }
     }
   }
 
-  jnll = sum(nll)
   jnll = sum(nll)
 
   # reporting
@@ -246,8 +207,6 @@ obj = MakeADFun(f, par, map = map)
 obj$fn()
 obj$gr()
 
-opt = nlminb(obj$par, obj$fn, obj$gr, control = list(eval.max = 1e3, iter.max = 1e3))
-sdr = sdreport(obj)
 opt = nlminb(obj$par, obj$fn, obj$gr, control = list(eval.max = 1e3, iter.max = 1e3))
 sdr = sdreport(obj)
 print(opt)
