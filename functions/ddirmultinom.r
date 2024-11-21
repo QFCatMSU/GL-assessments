d326e99b397c329520b78830aef46a54e31d1e12
Lisa Chong <32689355+lidach@users.noreply.github.com>
2024-11-20 22:49:06 -0500

    start of the semi-colons

--- functions/ddirmultinom.r
+++ functions/ddirmultinom.r
@@ -1,15 +1,15 @@
 # Dirichelt multinomial likelihood
 ddirmultinom = function(obs, pred, input_n, theta) 
 {
-  dir_param = theta * input_n
-  nll = lgamma(input_n + 1) + lgamma(dir_param) - lgamma(input_n + dir_param)
-  nll2 = 0
+  dir_param = theta * input_n;
+  nll = lgamma(input_n + 1) + lgamma(dir_param) - lgamma(input_n + dir_param);
+  nll2 = 0;
   for (a in 1:length(pred)) 
   {
     nll2 = nll2 - lgamma(obs[a] * input_n + 1) -
       lgamma(obs[a] * input_n + dir_param * pred[a]) +
-      lgamma(dir_param * pred[a])
+      lgamma(dir_param * pred[a]);
   }
-  nll = nll - nll2
-  return(nll)
+  nll = nll - nll2;
+  return(nll);
 }
